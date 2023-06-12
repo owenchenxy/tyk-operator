a3b16700be9f843816836f140409ab5430db0396
Xianyao Chen <owenchenxy@gmail.com>
2023-06-12 18:58:39 +0800

    add sec to app product

--- controllers/apidefinition_controller.go
+++ controllers/apidefinition_controller.go
@@ -718,7 +718,7 @@ func (r *ApiDefinitionReconciler) updateLinkedPolicies(ctx context.Context, a *t
 	}
 	allPolicies, _ := klient.Universal.Portal().Policy().All(ctx)
 	for i, p := range allPolicies {
-		fmt.Printf("==========%d===========: %v\n", i, p)
+		fmt.Printf("==========%d===========: %s, %s \n", i, p.Name, p.MID)
 	}
 
 	for k, x := range a.Spec.JWTScopeToPolicyMapping {
